0e57017c558c45f213340a42a562278cc4ae711e
Zsolt Makrai <mecrai@gmail.com>
2017-04-18 09:44:53 +0200

    fix(odd average): now really appends the list

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -6,9 +6,12 @@ def odd_average(list_of_numbers):
     odd_list = []
     for list_item in list_of_numbers:
         if list_item % 2 != 0:
-            odd_list.appned(list_item)
+            odd_list.append(list_item)
     sum_odd_numbers = 0
     for odd_list_item in odd_list:
         sum_odd_numbers += odd_list_item
     average = sum_odd_numbers / len(odd_list)
     return average
+
+numbers = [1, 2, 3, 4, 5, 10, 1]
+print(odd_average(numbers))
