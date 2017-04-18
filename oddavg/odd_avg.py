56db80297f0a012a63eb2889bb8c8ef351b7bb8b
Zsolt Makrai <mecrai@gmail.com>
2017-04-18 09:41:01 +0200

    feat(odd average): function created

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,14 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def odd_average(list_of_numbers):
+    odd_list = []
+    for list_item in list_of_numbers:
+        if list_item % 2 != 0:
+            odd_list.appned(list_item)
+    sum_odd_numbers = 0
+    for odd_list_item in odd_list:
+        sum_odd_numbers += odd_list_item
+    average = sum_odd_numbers / len(odd_list)
+    return average
