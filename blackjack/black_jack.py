821de4a6323ffb5c241f310807995d89763115cd
Zsolt Makrai <mecrai@gmail.com>
2017-04-18 11:09:07 +0200

    feat(blackjack): card and dack class created

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -10,13 +10,35 @@
 # We should be able to draw the top card which returns the drawn card and also removes it from the deck
 # Deck should be represented as string in this format:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+
+class Card(object):
+    def __init__(self, color, value):
+        self.color = color
+        self.value = value
+
+    def display_card(self):
+        print (str(self.value) + self.color)
+
+class Deck(object):
+    def __init__(self, number_of_cards):
+        self.number_of_cards = int(number_of_cards)
+        self.card_list = []
+        self.fill_deck()
+
+    def fill_deck(self):
+        if self.number_of_cards > 4:
+            quater_deck = self.number_of_cards // 4
+            print(quater_deck)
+        else:
+            pass
+
 deck = Deck(12)
 print(deck)
 # Should print out:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
-top_card = deck.draw()
-print(top_card)
-print(deck)
+# top_card = deck.draw()
+# print(top_card)
+# print(deck)
 # Should print out:
 # Queen Spades
 # 11 cards - 3 Clubs, 3 Diamonds, 3 Hearts, 2 Spades
