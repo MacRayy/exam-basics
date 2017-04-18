95166d6e57b1af1acb7aeb0454929cd33816bd84
Zsolt Makrai <mecrai@gmail.com>
2017-04-18 10:18:11 +0200

    feat(copy): prints help)

--- copy/copy.py
+++ copy/copy.py
@@ -6,3 +6,17 @@
 # No destination provided
 # When both arguments provided and the source is a file
 # Read all contents from it and write it to the destination
+import sys
+
+class Controller(object):
+    def argument_reader(self):
+        help_display = Display()
+        if len(sys.argv) == 1:
+            help_display.help_text()
+
+class Display(object):
+    def help_text(self):
+        return print("copy [source] [destination]")
+
+run_copy = Controller()
+run_copy.argument_reader()
